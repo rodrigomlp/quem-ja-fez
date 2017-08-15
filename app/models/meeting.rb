aa40c6d0af2208a4e7dbef63554961dc915689a9
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2017-08-15 17:12:57 -0300

    Criação de seeds (WIP)

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,6 +1,6 @@
 class Meeting < ApplicationRecord
 
-  belongs_to :undergraduate, class_name: 'User'#, foreign_key: 'undergraduate_id'
-  belongs_to :highschooler, class_name: 'User'#, foreign_key: 'highschooler_id'
+  belongs_to :undergraduate, class_name: 'User'
+  belongs_to :highschooler, class_name: 'User'
 
 end
--- app/models/university.rb
+++ app/models/university.rb
@@ -1,3 +1,3 @@
 class University < ApplicationRecord
-  belongs_to :resume
+  has_many :resumes
 end
