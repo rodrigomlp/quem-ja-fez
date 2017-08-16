2f0835f49856cca376fa6e28d2ea136d9bdb1862
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2017-08-16 16:19:39 -0300

    Now is possible for an undergraduate to view its resumes and add resumes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/show'
-
-  get 'users/index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' } # ~> Facebook connection
