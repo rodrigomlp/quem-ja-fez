2c9208b0890cd2e9449ad6a55edc336443b1a221
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2017-08-16 10:55:14 -0300

    Schema depois de rails db:migrate:reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170815210434) do
+ActiveRecord::Schema.define(version: 20170815224228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +55,6 @@ ActiveRecord::Schema.define(version: 20170815210434) do
   end
 
   create_table "users", force: :cascade do |t|
-    # Devise
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -82,7 +80,6 @@ ActiveRecord::Schema.define(version: 20170815210434) do
     t.string "last_name"
     t.string "token"
     t.datetime "token_expiry"
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
