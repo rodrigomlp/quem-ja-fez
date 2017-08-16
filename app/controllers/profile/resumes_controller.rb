0cf4294845416613e3d7120b9573e78d4576258b
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2017-08-16 19:11:40 -0300

    Now it is possible for a Undergrad to create, edit and delete a resume

--- app/controllers/profile/resumes_controller.rb
+++ app/controllers/profile/resumes_controller.rb
@@ -1,6 +1,7 @@
 class Profile::ResumesController < ApplicationController
   before_action :authenticate_user!
   before_action :check_if_user_is_undergraduate
+  before_action :set_resume, only: [:update, :destroy]
 
   def index
     @resumes = current_user.resumes
@@ -19,16 +20,40 @@ class Profile::ResumesController < ApplicationController
   end
 
   def update
+    if @resume.email_checked
+      if @resume.update(resume_params_for_checked_resume)
+        redirect_to profile_resumes_path
+      else
+        render :index
+      end
+    else
+      if @resume.update(resume_params)
+        redirect_to profile_resumes_path
+      else
+        render :index
+      end
 
+      # TO-DO: fazer ação que envia check de e-mail
+
+    end
   end
 
   def destroy
-
+    @resume.destroy
+    redirect_to profile_resumes_path
   end
 
   private
 
   def resume_params
     params.require(:resume).permit(:university_id, :course_id, :school_email, :relative_completion, :stance, :academic_description)
+  end
+
+  def resume_params_for_checked_resume
+    params.require(:resume).permit(:relative_completion, :stance, :academic_description)
+  end
+
+  def set_resume
+    @resume = Resume.find(params[:id])
   end
 end
