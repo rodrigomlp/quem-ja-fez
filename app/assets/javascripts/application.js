c18d6c7dadabf36f94042c3cb51e53f8e8a1d6b5
Rodrigo Proenca <rodrigomlp@gmail.com>
2017-08-17 15:48:58 -0300

    Calendario funcionando sem modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,41 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require momentjs
+//= require fullcalendar
 //= require_tree .
+
+
+
+
+
+
+$(document).ready(function(){
+  $('.calendar').each(function(){
+    var calendar = $(this);
+    calendar.fullCalendar({
+      header:{
+        left: 'prev,next,today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay'
+      },
+      selectable: true,
+      selectHelper: true,
+      editable: true,
+      eventLimit: true
+
+     // events: '/events.json',
+
+      // select: function(start, end) {
+      //   $.getScript('/events/new', function() {
+      //     $('#event_date_range').val(moment(start).format("MM/DD/YYYY HH:mm") + ' - ' + moment(end).format("MM/DD/YYYY HH:mm"))
+      //     date_range_picker();
+      //     $('.start_hidden').val(moment(start).format('YYYY-MM-DD HH:mm'));
+      //     $('.end_hidden').val(moment(end).format('YYYY-MM-DD HH:mm'));
+      //   });
+
+
+
+    });
+  });
+});
