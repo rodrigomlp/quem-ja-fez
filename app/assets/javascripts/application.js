0bad2d32d8b3e713b8010ae3d3d60aa9f49e8d8d
Rodrigo Proenca <rodrigomlp@gmail.com>
2017-08-17 16:41:18 -0300

    adding features

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,22 +17,25 @@ $(document).ready(function(){
       header:{
         left: 'prev,next,today',
         center: 'title',
-        right: 'month,agendaWeek,agendaDay'
+        right: 'agendaWeek'
       },
       selectable: true,
       selectHelper: true,
       editable: true,
-      eventLimit: true
+      eventLimit: true,
 
      // events: '/events.json',
 
-      // select: function(start, end) {
+      select: function(start, end) {
+        console.log("OLA")
+        console.log(start)
+        console.log(end)
       //   $.getScript('/events/new', function() {
       //     $('#event_date_range').val(moment(start).format("MM/DD/YYYY HH:mm") + ' - ' + moment(end).format("MM/DD/YYYY HH:mm"))
       //     date_range_picker();
       //     $('.start_hidden').val(moment(start).format('YYYY-MM-DD HH:mm'));
       //     $('.end_hidden').val(moment(end).format('YYYY-MM-DD HH:mm'));
-      //   });
+      }
 
 
 
