ebd46a0fdc39d93032dded010763e7d8e7790fe3
Luciano Krebs <luciano.krebs@hotmail.com>
2017-08-17 16:15:10 -0300

    creating the show page for each meeting

--- app/controllers/profile/meetings_controller.rb
+++ app/controllers/profile/meetings_controller.rb
@@ -1,5 +1,6 @@
 class Profile::MeetingsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_meeting, only: [:show, :edit, :update]
 
   # lógica ainda não terminada
   def index
@@ -9,5 +10,35 @@ class Profile::MeetingsController < ApplicationController
     @future_meetings_proposed = @meetings_accepted.where('start_time > ?', Time.now)
     @future_meetings = @future_meetings_accepted + @future_meetings_proposed
     @future_meetings.sort! { |a, b|  a.start_time <=> b.start_time }
+
   end
+
+
+  def show
+    @undergraduate = User.find(@meeting.undergraduate_id)
+    @highschooler = User.find(@meeting.undergraduate_id)
+  end
+
+  def new
+    @meeting = Meeting.new
+  end
+
+  def update
+    if @meeting.update(meeting_params)
+      redirect_to profile_meeting_path(@meeting)
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def meeting_params
+    params.require(:meeting).permit(:review_title, :review_content)
+  end
+
+  def set_meeting
+    @meeting = Meeting.find(params[:id])
+  end
+
 end
