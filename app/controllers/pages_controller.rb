a6f1ddbfc5b090f23412d40deab45a2511a440c2
marco beduschi <beduschimarco@gmail.com>
2017-08-18 16:00:54 -0300

    hotfix: banner_form now displays search result

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :about, :info]
 
   def home # home
+    @university = "ex.: USP"
+    @course = "ex.: Direito"
   end
 
   def about # about us
@@ -9,4 +11,8 @@ class PagesController < ApplicationController
 
   def info # undergrad sign up explanation page
   end
+
+  private
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,10 @@ class UsersController < ApplicationController
   end
 
   def index
+    # Displays this on banner form
+    @university = params[:university]
+    @course = params[:course]
+
     @resumes = Resume.all
     @resumes = @resumes.joins(:course, :university, :user) # joins all tables onto resume
     @resumes = @resumes.where(email_checked: true) # only show resumes that have been verified
