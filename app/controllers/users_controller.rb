4125522ba09800c7acfcde324bff829cfb7c54ff
Luciano Krebs <luciano.krebs@hotmail.com>
2017-08-18 21:21:10 -0300

    adicionando stars e progress bar para o show e index dos undergrads

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,5 +43,6 @@ class UsersController < ApplicationController
     if params[:course].present? # has the user entered anything in the 'course' search field?
       @resumes = @resumes.where("LOWER(courses.name) ILIKE ?", "%#{params[:course]}%")
     end
+
   end
 end
