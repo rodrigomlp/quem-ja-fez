551d3a38678d429720dfa6fa7becf69a680e8f0a
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2017-08-19 17:12:51 -0300

    gem instalada, configurado o css e o javascript file. Proximo pasos passo criar a seaarch form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,10 @@
 //= require bootstrap-sprockets
 //= require momentjs
 //= require fullcalendar
+
+// Gem usada para fazer o autocomplete da searchbar
+//= require select2
+
 //= require_tree .
 
 
