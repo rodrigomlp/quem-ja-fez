bc2d5ba87f38bd6628b891824aee60e156b78619
marco beduschi <beduschimarco@gmail.com>
2017-08-22 18:20:59 -0300

    added javascript to home page, changed navbar for the home page only

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,14 +4,12 @@
 //= require momentjs
 //= require fullcalendar
 //= require select
+//= require pages/home
 
 // Gem usada para fazer o autocomplete da searchbar
 //= require select2
 
 //= require_tree .
-
-
-
 
 
 
