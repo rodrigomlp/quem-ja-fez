2d57317f7c1651837e178bc5a13a5cc4127280c7
Luciano Krebs <luciano.krebs@hotmail.com>
2017-08-22 16:22:37 -0300

    testing heroku task

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -13,6 +13,18 @@ class Meeting < ApplicationRecord
     self.resume.course.name
   end
 
+  def send_emails
+    @next_meetings = Meeting.where('start_time >= ? AND end_time <= ?', Date.tomorrow.beginning_of_day, Date.tomorrow.end_of_day)
+    puts "The number of meetings in the next 24 hours is #{@next_meetings.size}"
+
+    # Disparando email para os users das next meetings
+    @next_meetings.each_with_index do |meeting, index|
+      puts "Sending emails for the meeting #{index + 1}"
+      UserMailer.remember_highschooler(meeting).deliver_now
+      UserMailer.remember_undergraduate(meeting).deliver_now
+    end
+  end
+
   private
 
   def create_virtual_room
