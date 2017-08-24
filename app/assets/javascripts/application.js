faca6a34fdadde7dfded3677856abd698467faec
Rodrigo Proenca <rodrigomlp@gmail.com>
2017-08-24 17:04:36 -0300

    Modificação temporário no footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,12 +3,10 @@
 //= require bootstrap-sprockets
 //= require momentjs
 //= require fullcalendar
-//= require select
 //= require pages/home
 //= require parallax
 //= require jquery.parallax
 
 // Gem usada para fazer o autocomplete da searchbar
-//= require select2
 
 //= require_tree .
--- app/assets/javascripts/select.js
+++ app/assets/javascripts/select.js
@@ -1,15 +1,17 @@
  // This allows our search bar to be searchable withing the options presented
+
  $(document).ready(function() {
-    $(".select-course-w-search").select2({
+    // $(".select-course-w-search").select2();
+    $("#course").select2({
       placeholder: "Selecione um curso",
       allowClear: true,
       theme: "bootstrap",
-      width: "225",
+      width: "225"
     });
   });
 
  $(document).ready(function() {
-    $(".select-university-w-search").select2({
+    $("#university").select2({
       placeholder: "Selecione uma universidade",
       allowClear: true,
       theme: "bootstrap",
