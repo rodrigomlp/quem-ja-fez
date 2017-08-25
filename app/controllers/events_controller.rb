a8541ac0c3642964c7bb71470513fb52dda3bc1a
marco beduschi <beduschimarco@gmail.com>
2017-08-24 23:48:49 -0300

    added stars to confirm page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -52,7 +52,21 @@ class EventsController < ApplicationController
     @resume = Resume.find(params[:user_id])
     @user = @resume.user
     @events = @user.events.where(color: "red")
+    @meetings = Meeting.where(undergraduate: @resume.user)
     @meeting = Meeting.new
+
+    meetings_rated = 0
+    total_rating = 0
+
+
+    @meetings.each do |meeting|
+      if meeting.rating.present?
+        total_rating += meeting.rating
+        meetings_rated += 1
+      end
+    end
+
+    @avg_rating = meetings_rated.zero? ? nil : round_point5(total_rating.to_f / meetings_rated)
   end
 
   private
@@ -62,6 +76,12 @@ class EventsController < ApplicationController
 
     def event_params
       params.require(:event).permit(:title, :start, :end, :color, :user_id)
+    end
+
+    private
+
+    def round_point5(number)
+      (number*2).round / 2.0
     end
 
 end
