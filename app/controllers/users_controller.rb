fed92d55db30f12145b812c77c4de272c9849a3e
Luciano Krebs <luciano.krebs@hotmail.com>
2017-08-24 21:16:13 -0300

    fixing search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
     @resumes = @resumes.where(email_checked: true) # only show resumes that have been verified
 
     if params[:university].present? # has the user entered anything in the 'university' search field?
-      @resumes = @resumes.where("LOWER(universities.name) ILIKE ?", "%#{University.find(params[:university])}%") #PERGUNTA: por que o ilike e nao só like? O que é esse lower?
+      @resumes = @resumes.where("LOWER(universities.name) ILIKE ?", "%#{University.find(params[:university]).name.downcase}%") #PERGUNTA: por que o ilike e nao só like? O que é esse lower?
     end
     if params[:course].present? # has the user entered anything in the 'course' search field?
       @resumes = @resumes.where("LOWER(courses.name) ILIKE ?", "%#{Course.find(params[:course]).name.downcase}%")
